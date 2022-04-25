f86d344a42814d5f1405084868f6a89d472b0260
hinam <hinam@LAPTOP-PLMBVL5O>
2022-04-25 11:53:24 +0530

    By Heena25

--- GitDemo/src/Demo.java
+++ GitDemo/src/Demo.java
@@ -1,8 +1,16 @@
 
 public class Demo {
+	
+	public void m1()
+	{
+		System.out.println("This is M1 method");
+	}
 
 	public static void main(String[] args) {
 		System.out.println("Hello");
+		Demo d =new Demo();
+		d.m1();
+		
 
 	}
 
