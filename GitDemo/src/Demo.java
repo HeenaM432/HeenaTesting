d46907b74c998f8eaa032d5e1cd5d92e6960c0e0
HeenaM432 <104188603+HeenaM432@users.noreply.github.com>
2022-04-25 12:28:08 +0530

    Branch1

--- GitDemo/src/Demo.java
+++ GitDemo/src/Demo.java
@@ -9,6 +9,10 @@ public class Demo {
 	{
 		System.out.println("This is M2 method");
 	}
+	public void m3()
+	{
+		System.out.println("This is M3 method");
+	}
 
 	public static void main(String[] args) {
 		System.out.println("Hello");
