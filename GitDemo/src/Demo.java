a980ae6b98a411eba3c4744510639bf98c636b79
HeenaM432 <104188603+HeenaM432@users.noreply.github.com>
2022-04-25 11:56:04 +0530

    Update Demo.java

--- GitDemo/src/Demo.java
+++ GitDemo/src/Demo.java
@@ -5,6 +5,10 @@ public class Demo {
 	{
 		System.out.println("This is M1 method");
 	}
+	public void m2()
+	{
+		System.out.println("This is M2 method");
+	}
 
 	public static void main(String[] args) {
 		System.out.println("Hello");
